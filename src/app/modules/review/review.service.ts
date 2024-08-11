2beae85871f06ad44fd7fe7716b36b45efb9acb9
Mohammad Arif <programmermdarif@gmail.com>
2024-08-11 18:59:55 +0600

    Updated the review api

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -1,20 +1,18 @@
-import httpStatus from "http-status"
-import AppError from "../../errors/AppError"
-import { BlogModel } from "../blog/blog.model"
-import { TReview } from "./review.interface"
-import { ReviewModel } from "./review.model"
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { BlogModel } from '../blog/blog.model';
+import { TReview } from './review.interface';
+import { ReviewModel } from './review.model';
 
-const createReviewIntoDB = async(payload:TReview)=>{
-    const checkData = await BlogModel.find({_id: payload.blogId , author:payload.userId })
-    if(!checkData){
-       throw new AppError(httpStatus.NOT_FOUND,"This blog doesn't exits")
-    }
-   const result = await ReviewModel.create(payload)
-   return result
-}
-
-
+const createReviewIntoDB = async (payload: TReview) => {
+  const checkData = await BlogModel.find({ _id: payload.blogId });
+  if (!checkData) {
+    throw new AppError(httpStatus.NOT_FOUND, "This blog doesn't exits");
+  }
+  const result = await ReviewModel.create(payload);
+  return result;
+};
 
 export const reviewService = {
-    createReviewIntoDB
-}
+  createReviewIntoDB,
+};
